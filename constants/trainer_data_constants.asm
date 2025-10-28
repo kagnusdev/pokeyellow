f304b7514dae2451512eacde8190c270c93b0428
kagnus <kagnusdev@gmail.com>
2025-10-28 09:31:33 +0100

    Step 8: Trainer data: Add support for mon nicknames

--- constants/trainer_data_constants.asm
+++ constants/trainer_data_constants.asm
@@ -3,5 +3,6 @@
 	shift_const TRAINERTYPE_MOVES     ; bit 0
 	shift_const TRAINERTYPE_DVS       ; bit 1
 	shift_const TRAINERTYPE_STAT_EXP  ; bit 2
+	shift_const TRAINERTYPE_NICKNAMES ; bit 3
 ; legacy value that is not simple flags
 DEF TRAINERTYPE_MULTI_LEVELS EQU $FF
